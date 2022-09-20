ac77183397896472594e3b08018b7f4b0b8dfcc9
Rifqi Fathurrahman <52643106+rifqifathur261@users.noreply.github.com>
2022-09-20 18:45:29 +0700

    First Commit

---
+++ Assets/Script/Enemy.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float speed;
+    public Transform target;
+    private Rigidbody2D rb;
+    public float stoppingDistance;
+
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody2D>();
+        //target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+
+    void Update()
+    {
+        Vector3 direction = target.position - transform.position;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg - 90f;
+        rb.rotation = angle;
+
+        //majuin musuh ke player
+        if(Vector2.Distance(transform.position, target.position) > stoppingDistance)
+        transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);     
+    }
+}
